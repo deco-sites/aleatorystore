0b1f17e40b0c5f744ee066eb32ca06c6bbc299e3
Luiz Felipe <luizfelipebragaa@gmail.com>
2024-07-23 10:27:42 -0300

    feat: category banner

--- components/ui/CategoryBanner.tsx
+++ components/ui/CategoryBanner.tsx
@@ -51,21 +51,23 @@ function Banner(props: SectionProps<ReturnType<typeof loader>>) {
 
   return (
     <div class="grid grid-cols-1 grid-rows-1">
-      <Picture preload class="col-start-1 col-span-1 row-start-1 row-span-1">
-        <Source
-          src={image.mobile}
-          width={360}
-          height={120}
-          media="(max-width: 767px)"
-        />
-        <Source
-          src={image.desktop}
-          width={1440}
-          height={200}
-          media="(min-width: 767px)"
-        />
-        <img class="w-full" src={image.desktop} alt={image.alt ?? title} />
-      </Picture>
+      <figure class="relative" style={{ aspectRatio: "1341/467" }}>
+        <Picture preload class="col-start-1 col-span-1 row-start-1 row-span-1">
+          <Source
+            src={image.mobile}
+            width={360}
+            height={120}
+            media="(max-width: 767px)"
+          />
+          <Source
+            src={image.desktop}
+            width={1341}
+            height={467}
+            media="(min-width: 767px)"
+          />
+          <img class="w-full" src={image.desktop} alt={image.alt ?? title} />
+        </Picture>
+      </figure>
 
       <div class="container flex flex-col items-center justify-center sm:items-start col-start-1 col-span-1 row-start-1 row-span-1 w-full">
         <h1>
