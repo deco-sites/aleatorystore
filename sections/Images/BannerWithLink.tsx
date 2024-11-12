094e02b3ae0a748730ad7fa8eb147319325263c8
Guilherme da Silva Benevides <git@gui.dev.br>
2024-11-12 16:24:12 -0300

    feat: banner

--- sections/Images/BannerWithLink.tsx
+++ sections/Images/BannerWithLink.tsx
@@ -2,37 +2,45 @@ import { ImageWidget } from "apps/admin/widgets.ts";
 import { Picture, Source } from "apps/website/components/Picture.tsx";
 
 interface Props {
-    desktop: ImageWidget;
-    mobile: ImageWidget;
+    desktop: {
+        src: ImageWidget;
+        width: number;
+        height: number;
+    };
+    mobile: {
+        src: ImageWidget;
+        width: number;
+        height: number;
+    };
     alt: string;
     url: string;
 }
 
-export default function Banner(props: Props) {
+export default function BannerWithLink(props: Props) {
     const { desktop, mobile, alt, url } = props;
     return (
-        <a href={url}>
+        <a href={url} class="mt-10 block">
             <Picture>
                 <Source
                     media="(max-width: 767px)"
                     fetchPriority="auto"
-                    src={mobile}
-                    width={390}
-                    height={614}
+                    src={mobile.src}
+                    width={mobile.width}
+                    height={mobile.height}
                 />
                 <Source
                     media="{min-width: 768px}"
                     fetchPriority="auto"
-                    src={desktop}
-                    width={1920}
-                    height={907}
+                    src={desktop.src}
+                    width={desktop.width}
+                    height={desktop.height}
                 />
                 <img
                     class="object-fill w-full"
                     loading="lazy"
-                    width={344}
-                    height={500}
-                    src={desktop}
+                    src={desktop.src}
+                    width={desktop.width}
+                    height={desktop.height}
                     alt={alt}
                 />
             </Picture>
