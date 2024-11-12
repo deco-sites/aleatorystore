1e5d7f2501575d907e042b3c273018330c24ba9c
Guilherme da Silva Benevides <git@gui.dev.br>
2024-11-12 07:59:27 -0300

    feat: save changes

--- components/product/ProductShelfTabbed.tsx
+++ components/product/ProductShelfTabbed.tsx
@@ -1,82 +1,238 @@
+import { SectionProps } from "@deco/deco";
+import { usePartialSection } from "@deco/deco/hooks";
 import type { Product } from "apps/commerce/types.ts";
 import { mapProductToAnalyticsItem } from "apps/commerce/utils/productToAnalyticsItem.ts";
+import { AppContext } from "../../apps/site.ts";
 import { SendEventOnView } from "../../components/Analytics.tsx";
 import ProductCard from "../../components/product/ProductCard.tsx";
 import Icon from "../../components/ui/Icon.tsx";
 import Header from "../../components/ui/SectionHeader.tsx";
 import Slider from "../../components/ui/Slider.tsx";
+import { clx } from "../../sdk/clx.ts";
 import { useId } from "../../sdk/useId.ts";
 import { useOffer } from "../../sdk/useOffer.ts";
 import { usePlatform } from "../../sdk/usePlatform.tsx";
-import { usePartialSection } from "@deco/deco/hooks";
+import ButtonBanner from "../ui/ButtonBanner.tsx";
 /** @titleBy title */
 interface Tab {
-    title: string;
-    products: Product[] | null;
+  title: string;
+  products: Product[] | null;
 }
 export interface Props {
-    tabs: Tab[];
-    title?: string;
-    description?: string;
-    layout?: {
-        headerAlignment?: "center" | "left";
-        headerfontSize?: "Normal" | "Large";
+  tabs: Tab[];
+  title?: string;
+  description?: string;
+  layout?: {
+    numberOfSliders?: {
+      mobile?: 1 | 2 | 3 | 4 | 5;
+      desktop?: 1 | 2 | 3 | 4 | 5;
     };
-    tabIndex?: number;
+    headerAlignment?: "center" | "left";
+    headerfontSize?: "Normal" | "Large" | "Small";
+    showArrows?: boolean;
+    showDots?: boolean;
+    collectionButton?: {
+      show?: boolean;
+      action?: {
+        text: string;
+        href: string;
+      };
+    };
+  };
+  tabIndex?: number;
 }
-function TabbedProductShelf({ tabs, title, description, layout, tabIndex, }: Props) {
-    const id = useId();
-    const platform = usePlatform();
-    const ti = typeof tabIndex === "number"
-        ? Math.min(Math.max(tabIndex, 0), tabs.length)
-        : 0;
-    const { products } = tabs[ti];
-    if (!products || products.length === 0) {
-        return null;
-    }
-    return (<div class="w-full container  py-8 flex flex-col gap-8 lg:gap-12 lg:py-10">
-      <Header title={title || ""} description={description || ""} fontSize={layout?.headerfontSize || "Large"} alignment={layout?.headerAlignment || "center"}/>
+
+export const loader = (props: Props, req: Request, ctx: AppContext) => {
+  const url = new URL(req.url);
+  return {
+    ...props,
+    device: ctx.device,
+    url: url,
+  };
+};
+function TabbedProductShelf(
+  { tabs, title, description, layout, tabIndex, url }: SectionProps<
+    typeof loader
+  >,
+) {
+  const id = useId();
+  const platform = usePlatform();
+  const ti = typeof tabIndex === "number"
+    ? Math.min(Math.max(tabIndex, 0), tabs.length)
+    : 0;
+  const { products } = tabs[ti];
+  if (!products || products.length === 0) {
+    return null;
+  }
+  const isProductPage = url?.searchParams.has("skuId");
+  const slideDesktop = {
+    1: "md:w-full",
+    2: "md:w-1/2",
+    3: "md:w-[28%]",
+    4: "md:w-1/4",
+    5: "md:w-1/5",
+  };
+  const slideMobile = {
+    1: "w-[90%]",
+    2: "w-1/2",
+    3: "w-1/3",
+    4: "w-1/4",
+    5: "w-1/5",
+  };
+  return (
+    <div
+      class={clx(
+        "container w-full pt-8 flex flex-col gap-6 lg:pt-10",
+        isProductPage ? "lg:px-10 2xl:pl-20" : "",
+        layout?.showArrows ? "lg:pb-[69px]" : "pb-8 lg:pb-10",
+      )}
+    >
+      <Header
+        title={title || ""}
+        description={description || ""}
+        fontSize={layout?.headerfontSize || "Large"}
+        alignment={layout?.headerAlignment || "center"}
+      />
 
       <div class="flex justify-center">
         <div class="tabs tabs-boxed">
-          {tabs.map((tab, index) => (<button class={`tab tab-lg ${index === ti ? "tab-active" : ""}`} {...usePartialSection({ props: { tabIndex: index } })}>
+          {tabs.map((tab, index) => (
+            <button
+              class={`tab tab-lg ${index === ti ? "tab-active" : ""}`}
+              {...usePartialSection({ props: { tabIndex: index } })}
+            >
               {tab.title}
-            </button>))}
+            </button>
+          ))}
         </div>
       </div>
 
-      <div id={id} class="container grid grid-cols-[48px_1fr_48px] px-0 sm:px-5">
-        <Slider class="carousel carousel-center sm:carousel-end gap-6 col-span-full row-start-2 row-end-5">
-          {products?.map((product, index) => (<Slider.Item index={index} class="carousel-item w-[270px] sm:w-[292px] first:pl-6 sm:first:pl-0 last:pr-6 sm:last:pr-0">
-              <ProductCard product={product} itemListName={title} platform={platform} index={index}/>
-            </Slider.Item>))}
+      <div
+        id={id}
+        class={clx(
+          "relative grid",
+          layout?.showArrows &&
+            "grid-cols-[0px_1fr_0px] lg:grid-cols-[0px_1fr_0px] ml-4 sm:ml-0",
+        )}
+      >
+        <Slider class="relative carousel carousel-center col-start-2 col-end-2 row-start-1 row-end-4 gap-1 pr-4 sm:pr-0">
+          {products?.map((product, index) => (
+            <Slider.Item
+              index={index}
+              class={clx(
+                "carousel-item",
+                slideDesktop[layout?.numberOfSliders?.desktop ?? 3],
+                slideMobile[layout?.numberOfSliders?.mobile ?? 1],
+              )}
+            >
+              <ProductCard
+                product={product}
+                itemListName={title}
+                platform={platform}
+                index={index}
+              />
+            </Slider.Item>
+          ))}
         </Slider>
 
-        <>
-          <div class="hidden relative sm:block z-10 col-start-1 row-start-3">
-            <Slider.PrevButton class="btn btn-circle btn-outline absolute right-1/2 bg-secondary-neutral-100">
-              <Icon size={24} id="ChevronLeft" strokeWidth={3}/>
-            </Slider.PrevButton>
+        {layout?.showArrows && !layout?.collectionButton?.show && (
+          <div class="absolute bottom-[-20px] lg:bottom-0 right-20 w-full lg:w-[250px]">
+            <div class="relative left-[80px] lg:left-auto lg:right-[187px] block z-10">
+              <Slider.PrevButton class="absolute h-12 flex justify-center items-center top-3 lg:top-[27px]">
+                <Icon
+                  size={24}
+                  id="ChevronLeft"
+                  strokeWidth={0.01}
+                  class="w-5"
+                />
+              </Slider.PrevButton>
+            </div>
+
+            <ul
+              class={`absolute left-[110px] lg:left-auto lg:right-0 carousel grid grid-cols-${products.length} mt-[35px] lg:mt-[50px] items-end col-span-full z-10 row-start-4 w-[calc(100%-76px)] lg:w-[400px] m-auto bg-secondary-neutral-600`}
+            >
+              {products?.map((_, index) => (
+                <li class="carousel-item w-full">
+                  <Slider.Dot index={index} class="w-full">
+                    <div class="w-full h-[2px] group-disabled:bg-dark-blue bg-transparent" />
+                  </Slider.Dot>
+                </li>
+              ))}
+            </ul>
+
+            <div class="absolute right-[-42px] lg:right-0 block z-10">
+              <Slider.NextButton class="absolute h-12 flex justify-center items-center top-3 lg:top-[27px] lg:left-[15px]">
+                <Icon size={24} id="ChevronRight" strokeWidth={0.01} />
+              </Slider.NextButton>
+            </div>
           </div>
-          <div class="hidden relative sm:block z-10 col-start-3 row-start-3">
-            <Slider.NextButton class="btn btn-circle btn-outline absolute left-1/2 bg-secondary-neutral-100">
-              <Icon size={24} id="ChevronRight" strokeWidth={3}/>
-            </Slider.NextButton>
+        )}
+
+        {layout?.showArrows && layout?.collectionButton?.show && (
+          <>
+            <div class="relative block z-10 col-start-1 row-start-3 left-[-13px] lg:left-auto">
+              <Slider.PrevButton class="absolute w-12 h-12 flex justify-center items-center top-3 lg:top-[27px]">
+                <Icon
+                  size={24}
+                  id="ChevronLeft"
+                  strokeWidth={0.01}
+                  class="w-5"
+                />
+              </Slider.PrevButton>
+            </div>
+            <div class="absolute col-start-3 right-16 lg:col-start-auto lg:left-[450px] lg:right-auto top-3 lg:top-[26px] block z-10 row-start-3">
+              <Slider.NextButton class="absolute w-12 h-12 flex justify-center items-center">
+                <Icon size={24} id="ChevronRight" strokeWidth={0.01} />
+              </Slider.NextButton>
+            </div>
+          </>
+        )}
+
+        {layout?.showDots && layout?.collectionButton?.show && (
+          <ul
+            class={`absolute left-[40px] lg:left-[50px] carousel grid grid-cols-${products.length} mt-[35px] lg:mt-[50px] items-end col-span-full z-10 row-start-4 w-[calc(100%-110px)] lg:w-[400px] m-auto bg-secondary-neutral-600`}
+          >
+            {products?.map((_, index) => (
+              <li class="carousel-item w-full">
+                <Slider.Dot index={index} class="w-full">
+                  <div class="w-full h-[2px] group-disabled:bg-dark-blue bg-transparent" />
+                </Slider.Dot>
+              </li>
+            ))}
+          </ul>
+        )}
+
+        {layout?.collectionButton?.show && (
+          <div class="absolute left-1/2 transform -translate-x-1/2 bottom-[-120px] lg:right-12 lg:left-auto lg:bottom-[-80px] lg:transform-none">
+            <a href={layout?.collectionButton.action?.href}>
+              <div class="-ml-8 md:ml-auto">
+                <ButtonBanner aria-label={layout.collectionButton.action?.text}>
+                  {layout?.collectionButton.action?.text}
+                </ButtonBanner>
+              </div>
+            </a>
           </div>
-        </>
-        <Slider.JS rootId={id}/>
-        <SendEventOnView id={id} event={{
+        )}
+
+        <Slider.JS rootId={id} />
+        <SendEventOnView
+          id={id}
+          event={{
             name: "view_item_list",
             params: {
-                item_list_name: title,
-                items: products.map((product, index) => mapProductToAnalyticsItem({
-                    index,
-                    product,
-                    ...(useOffer(product.offers)),
-                })),
+              item_list_name: title,
+              items: products.map((product, index) =>
+                mapProductToAnalyticsItem({
+                  index,
+                  product,
+                  ...(useOffer(product.offers)),
+                })
+              ),
             },
-        }}/>
+          }}
+        />
       </div>
-    </div>);
+    </div>
+  );
 }
 export default TabbedProductShelf;
---
+++ sections/Cards/CardShowcase.tsx
@@ -0,0 +1,86 @@
+import { ImageWidget } from "apps/admin/widgets.ts";
+import { Picture, Source } from "apps/website/components/Picture.tsx";
+import ButtonBanner from "../../components/ui/ButtonBanner.tsx";
+
+/**@title {{title}} */
+interface Card {
+  title: string;
+  description: string;
+  cta: {
+    text: string;
+    href: string;
+  };
+  image: {
+    desktop: ImageWidget;
+    mobile: ImageWidget;
+    /**@title Alternative Text */
+    alt: string;
+  };
+}
+interface Props {
+  /** @minItems 3  */
+  /** @maxItems 3 */
+  cards: Card[];
+}
+
+export default function CardShowcase({ cards }: Props) {
+  if (cards.length !== 3) return null;
+  return (
+    <div class="flex flex-col gap-5 lg:gap-0 lg:flex-row w-fit mx-auto mt-[60px] lg:mb-10">
+      {cards.map((card) => (
+        <article
+          key={card.title}
+          class="mx-4 uppercase lg:first:top-10 lg:last:top-10 relative lg:w-1/3 "
+        >
+          <a
+            href={card.cta.href}
+            class="flex flex-col items-center text-[#000]"
+          >
+            <div class="group overflow-hidden">
+              <Picture>
+                <Source
+                  media="(max-width: 767px)"
+                  fetchPriority="auto"
+                  src={card.image.mobile}
+                  width={334}
+                  height={500}
+                />
+                <Source
+                  media="{min-width: 768px}"
+                  fetchPriority="auto"
+                  src={card.image.desktop}
+                  width={344}
+                  height={500}
+                />
+                <img
+                  class="object-fill max-w-[334px] max-h-[500px] group-hover:scale-[1.12] transition-transform duration-300"
+                  loading="lazy"
+                  width={344}
+                  height={500}
+                  src={card.image.desktop}
+                  alt={card.image.alt}
+                />
+              </Picture>
+            </div>
+            <h3 class="font-normal text-2xl leading-6 mt-6">{card.title}</h3>
+            <p class="mt-3 font-light text-sm">{card.description}</p>
+            <ButtonBanner class="my-6" href={card.cta.href}>
+              {card.cta.text}
+            </ButtonBanner>
+          </a>
+        </article>
+      ))}
+    </div>
+  );
+}
+
+export function LoadingFallback() {
+  return (
+    <div
+      style={{ height: "710px" }}
+      class="w-full flex justify-center items-center"
+    >
+      <span class="loading loading-spinner" />
+    </div>
+  );
+}
