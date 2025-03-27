5132a7225cab4c04cf7eaad724846b0304cc3d00
Guilherme da Silva Benevides <git@gui.dev.br>
2025-03-27 13:55:45 -0300

    feat: benefit bar

--- sections/Benefits.tsx
+++ sections/Benefits.tsx
@@ -1,67 +1,48 @@
-import { AppContext } from "../apps/site.ts";
-import Icon from "../components/ui/Icon.tsx";
 import { useId } from "../sdk/useId.ts";
-import { type SectionProps } from "@deco/deco";
-export interface Benefit {
-  icon: "BenefitShoppingCart" | "BenefitCreditCard" | "BenefitShipping";
-  title: string;
-  /** @format html */
-  description: string;
-}
+
 const defaultValues: Benefit[] = [
   {
-    icon: "BenefitShoppingCart",
-    title: "1° Compra",
-    description:
-      "Ganhe 10% OFF na sua primeira compra no site. Já aplicado no carrinho",
+    title: "ATÉ 10X SEM JUROS",
   },
   {
-    icon: "BenefitShoppingCart",
-    title: "1° Compra",
-    description:
-      "Ganhe 10% OFF na sua primeira compra no site. Já aplicado no carrinho",
+    title: "FRETE GRÁTIS ACIMA DE R$ 499",
   },
   {
-    icon: "BenefitShoppingCart",
-    title: "1° Compra",
-    description:
-      "Ganhe 10% OFF na sua primeira compra no site. Já aplicado no carrinho",
+    title: "2.5% de CashBack",
+    description: "em todas as compras faturadas",
   },
 ];
+
+/** @titleBy title */
+export interface Benefit {
+  title: string;
+  description?: string;
+}
 export interface Props {
   items: Benefit[];
 }
-export const loader = (props: Props, req: Request, _ctx: AppContext) => {
-  const url = new URL(req.url);
-  return {
-    ...props,
-    url,
-  };
-};
+
 export default function Benefits(
-  { url, items = defaultValues }: SectionProps<typeof loader>,
+  { items = defaultValues }: Props,
 ) {
   const id = useId();
-  const isProductPage = url.searchParams.has("skuId");
   return (
-    <div
-      id={id}
-      class={`lg:container mx-6 lg:mx-auto lg:mb-20 grid gap-8 lg:gap-4 grid-cols-1 place-items-center lg:grid-cols-3 text-center px-4 ${
-        isProductPage ? "mt-28 lg:mt-28 mb-11" : "mb-11 mt-[55px]"
-      }`}
-    >
-      {items.map((item) => (
-        <div class="flex flex-col items-center justify-center max-w-72">
-          <Icon id={item.icon} size={40} strokeWidth={0.01} />
-          <h3 class="text-2xl my-1 text-primary-900 uppercase font-normal">
-            {item.title}
-          </h3>
-          <p
-            class=" text-primary-900 font-light"
-            dangerouslySetInnerHTML={{ __html: item.description }}
-          />
-        </div>
-      ))}
+    <div class="bg-[#000] text-[#fff] py-2">
+      <div
+        id={id}
+        class={`lg:container mx-6 lg:mx-auto grid gap-2 lg:gap-4 grid-cols-1 place-items-center lg:grid-cols-3 text-center px-4`}
+      >
+        {items.map((item) => (
+          <div class="flex flex-col items-center justify-center max-w-72">
+            <h3 class="text-lg my-1 uppercase font-normal">
+              {item.title}
+            </h3>
+            <p
+              dangerouslySetInnerHTML={{ __html: item.description ?? "" }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
