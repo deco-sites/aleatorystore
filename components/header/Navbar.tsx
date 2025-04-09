8151d7f4b623ff560718c812863e27c0fa02f46b
Guilherme da Silva Benevides <guilherme.benevides@econverse.com.br>
2025-04-09 18:05:36 +0000

    Publish 5 from staging into origin/main

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -136,7 +136,7 @@ function Navbar(
           {!buttons?.hideAccountButton && (
             <a
               class="flex items-center text-xs font-thin"
-              href="/login"
+              href="/account"
               aria-label="Account"
             >
               <div class="flex btn btn-circle btn-sm btn-ghost gap-1">
